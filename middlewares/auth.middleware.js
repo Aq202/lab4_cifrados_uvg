0f7f315eb7897e801fe6901279b86a17ea0e42f9
Pablo Zamora <77593400+pabloozamora@users.noreply.github.com>
2025-04-09 23:30:47 -0600

    [feat]: Generar par de llaves frontend

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -8,11 +8,13 @@ const authenticateToken = (req, res, next) => {
   const token = req.headers['authorization'];
 
   if (!token) {
+    res.statusMessage = 'Acceso denegado: token no proporcionado';
     return res.status(401).json({ message: 'Acceso denegado: token no proporcionado' });
   }
 
   jwt.verify(token, JWT_SECRET, (err, decoded) => {
     if (err) {
+      res.statusMessage = 'Token inválido';
       return res.status(403).json({ message: 'Token inválido' });
     }
     // Decoded contiene la carga útil que se firmó en el token, { id, email, password hasheada }
